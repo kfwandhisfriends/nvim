128d53b845de70048ab85306f750498c598fd327
kfw <2500097267@qq.com>
2025-01-08 23:54:52 +0100

    gruvbox

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,12 +1,14 @@
 return {
     -- the colorscheme should be available when starting Neovim
     {
-        "folke/tokyonight.nvim",
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
-        config = function()
-            -- load the colorscheme here
-            vim.cmd([[colorscheme tokyonight]])
-        end,
+      'sainnhe/gruvbox-material',
+      lazy = false,
+      priority = 1000,
+      config = function()
+        -- Optionally configure and load the colorscheme
+        -- directly inside the plugin declaration.
+        vim.g.gruvbox_material_enable_italic = true
+        vim.cmd.colorscheme('gruvbox-material')
+      end
     },
 }
